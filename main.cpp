ffddd64e71edc01a3921042d763d17c63245c177
Tim Robbins <GroggyAlgorithm@gmail.com>
2022-07-04 09:52:48 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,23 @@
-//Test file
+/**
+    \file main.cpp
+    \author Tim Robbins
+    \brief Test file, just to run examples from the random number generation. \n
+    This file is not proof of production ready software and is not intended to be used as such. \n
+    It runs, is not pretty, and was made with using the "copy and paste from other parts of the code" method.
+    
+    
+    
+    \todo I set the perlin function wrong and would hope to display using octaves to create managable noise. \n
+    Along with that, I think it's sorta annoying to have to go in and re-enter a lot of the information. Would be cool \n
+    if I added additional things with that in mind but I really do not want to dedicate too much time to the main file in this project. \n
+    Sometimes it's time to just move on.
+
+
+*/
+
+
+
+
 
 #include <cstdio>
 #include <iostream>
